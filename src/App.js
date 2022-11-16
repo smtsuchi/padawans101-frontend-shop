1856b90c7b9d728808b9a04e5f9c666ac7b82b75
smtsuchi <shoha1999@gmail.com>
2022-11-15 22:15:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getDatabase, ref, set, child, get } from "firebase/database";
 
-function App() {
+const STRIPE_API_KEY = process.env.REACT_APP_STRIPE_KEY
+
+export default function App() {
+  const getUserFromLS = () => {
+    const found = localStorage.getItem('shop_user');
+    if (found) {
+      return JSON.parse(found)
+    }
+    return {}
+  };
+
+  const [user, setUser] = useState(getUserFromLS());
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({size: 0});
+
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  const createPopup = async () => {
+    const result = await signInWithPopup(auth, provider)
+    const user = result.user
+    localStorage.setItem('shop_user', JSON.stringify(user))
+    setUser(user)
+    getCart(user)
+  };
+
+  const logOut = () => {
+    localStorage.removeItem('shop_user')
+    setUser({})
+    setCart({size:0})
+  };
+
+
+  const getProducts = async () => {
+    const res = await fetch('https://api.stripe.com/v1/products', {
+      method: "GET",
+      headers: {Authorization: `Bearer ${STRIPE_API_KEY}`}
+    });
+    const data = await res.json();
+    console.log(data)
+    setProducts(data.data)
+  };
+
+  useEffect(()=>{getProducts()}, [])
+  useEffect(()=>{if (user.uid){getCart(user)}},[user])
+
+  const showProducts = () => {
+    return products.map(p=><div key={p.id} style={{width:"18rem", border:'1px solid grey'}}>
+      <h1>{p.name}</h1>
+      <p>{p.description}</p>
+      <button onClick={()=>{addToCart(p)}}>Add To Cart</button>
+    </div>)
+  };
+
+  const addToDB = (cart) => {
+    const db = getDatabase();
+    set(ref(db, `/cart/${user.uid}`), cart)
+  };
+
+  const getCart = async (user) => {
+    const dbRef = ref(getDatabase())
+    const snapshot = await get(child(dbRef, `/cart/${user.uid}`))
+    if (snapshot.exists()){
+      setCart(snapshot.val())
+    }
+  }
+
+  const addToCart = (item) => {
+    const newCart = {...cart};
+    if (item.id in newCart) {
+      newCart[item.id].qty++;
+    }
+    else {
+      newCart[item.id] = item
+      newCart[item.id].qty = 1
+    }
+    newCart.size++
+
+    setCart(newCart)
+    if (user.uid){
+      // updateDB
+      addToDB(newCart)
+    }
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <h1>{user.uid?user.displayName:"GUEST"} | {cart.size}</h1>
 
-export default App;
+      <div className='row'>
+        {showProducts()}
+      </div>
+
+      {user.uid?
+      <button onClick={logOut}>Log Out</button>
+      :
+      <button onClick={createPopup}>Sign In With Google</button>
+      }
+
+
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+import { initializeApp } from "firebase/app";
+
+const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY
+const FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+const FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID
+const FIREBASE_STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+const FIREBASE_MESSAGE_ID = process.env.REACT_APP_FIREBASE_MESSAGE_ID
+const FIREBASE_APP_ID = process.env.REACT_APP_FIREBASE_APP_ID
+const FIREBASE_DATABASE_URL = process.env.REACT_APP_FIREBASE_DATABASE_URL
+
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGE_ID,
+  appId: FIREBASE_APP_ID,
+  databaseUrl: FIREBASE_DATABASE_URL
+};
+
+
+initializeApp(firebaseConfig);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +35,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
